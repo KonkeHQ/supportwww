aa71761faaaee801822bb2b7f683c7a99dffd74f
adi <astroianu530@gmail.com>
2021-07-19 09:15:22 +0300

    konke

--- l10n/en-GB.ts
+++ l10n/en-GB.ts
@@ -1,10 +1,10 @@
 export default {
-    PAGE_TITLE: "Dot HQ Support",
+    PAGE_TITLE: "Konke HQ Support",
 
-    WELCOME_LANDING_MASTHEAD: "Welcome to Dot HQ Support",
+    WELCOME_LANDING_MASTHEAD: "Welcome to Konke HQ Support",
     WELCOME_LANDING_SEARCH_PLACEHOLDER: "How can we help you?",
 
-    HEADER_RETURN_TO_MAIN: "Go to <u>www.dothq.co</u>",
+    HEADER_RETURN_TO_MAIN: "Go to <u>www.atech17.ga/u>",
 
     PRODUCTS_LIST_TITLE: "Explore our products and services",
 }
